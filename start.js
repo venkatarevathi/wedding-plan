625195f114633fac556d0227d0174cb1c2fc8cba
venkatarevathi <revathinelakurthi@gmail.com>
2025-11-02 18:07:58 +0530

    Fix deployment configuration for Render

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
-const { spawn } = require('child_process');
 const path = require('path');
 
 // Change directory to backend and start the server
 const backendPath = path.join(__dirname, 'backend');
 process.chdir(backendPath);
-require('./backend/server.js');
+require(path.join(backendPath, 'server.js'));
